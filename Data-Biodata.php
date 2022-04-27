b0635dc4c4e976e14280202987ccbbcda3d50f6f
AfkrNaqy <99960796+AfkrNaqy@users.noreply.github.com>
2022-04-27 23:22:09 +0700

    Add files via upload

---
+++ Data-Biodata.php
@@ -0,0 +1,10 @@
+<?php
+    $nama="Muhammad Afkar Naqy";
+    $email="naqy22afka@gmail.com";
+    $alamat="Jl. raya Bangilan";
+    $lahir="19-April-2002";
+    $umur="20 Tahun";
+    $kelamin="Laki-laki";
+    $status="Mahasiswa";
+    $nohp="081252222530";
+?>
